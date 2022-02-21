4ef0a0da551b191a3a9d7d604e1eef0f68cc25c9
Joel Lim <joellimjieen@hotmail.com>
2022-02-21 11:19:34 +0800

    background assignment

---
+++ Background Assignment/HelloWorld.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/** 
+ * @title HelloWorld
+ * @dev Implements Hello World smart contract: store an unsigned integer and then retrieve it.
+ */
+contract HelloWorld {
+    uint value; // declares state variable called value of type uint
+
+    constructor(uint _value) { // constructor that sets initial value to _value
+        value = _value;
+    }
+
+    function set (uint x) public { // setter function that sets value to x
+        value = x;
+    }
+
+    function get() public view returns (uint) { // getter function that returns value
+        return value;
+    }
+}
